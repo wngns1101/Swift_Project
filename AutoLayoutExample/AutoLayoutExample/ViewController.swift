b678a63ec1d0c06ae9d2f4a757109bbd9baa0107
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 <wngns0812@gmail.com>
2023-01-07 23:44:41 +0900

    change background color application

---
+++ AutoLayoutExample/AutoLayoutExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleProject
+//
+//  Created by juhoon lee on 2023/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBOutlet weak var colorView: UIView!
+    
+
+    @IBAction func tapChangeColorButton(_ sender: UIButton) {
+        self.colorView.backgroundColor = UIColor.blue
+        print("색상 변경 버튼이 클릭되었음")
+    }
+}
---
+++ ExampleProject/AutoLayoutExample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  ExampleProject
+//
+//  Created by juhoon lee on 2023/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
