55dc091fc085a5e1128d3483431fc54ca5630c32
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 <wngns0812@gmail.com>
2023-02-01 13:56:36 +0900

    CH08_Basic_Project

--- COVID19/COVID19/CovidDetailViewController.swift
+++ COVID19/COVID19/CovidDetailViewController.swift
@@ -8,9 +8,22 @@ class CovidDetailViewController: UITableViewController {
     @IBOutlet weak var totalCaseCell: UITableViewCell!
     @IBOutlet weak var regionalOutbreakCell: UITableViewCell!
     @IBOutlet weak var overseasInflowCell: UITableViewCell!
+    var covidOverview: CovidOverview?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureView()
     }
     
+    func configureView(){
+        guard let covidOverview = self.covidOverview else {return}
+        self.title = covidOverview.countryName
+        self.newCaseCell.detailTextLabel?.text = "\(covidOverview.newCase)명"
+        self.totalCaseCell.detailTextLabel?.text = "\(covidOverview.totalCase)명"
+        self.recoveredCell.detailTextLabel?.text = "\(covidOverview.recovered)명"
+        self.deathCell.detailTextLabel?.text = "\(covidOverview.death)명"
+        self.percentageCell.detailTextLabel?.text = "\(covidOverview.percentage)%"
+        self.overseasInflowCell.detailTextLabel?.text = "\(covidOverview.newFcase)"
+        self.regionalOutbreakCell.detailTextLabel?.text = "\(covidOverview.newCcase)"
+    }
 }
--- COVID19/COVID19/ViewController.swift
+++ COVID19/COVID19/ViewController.swift
@@ -14,13 +14,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var newCaseLabel: UILabel!
     @IBOutlet weak var totalCaseLabel: UILabel!
     @IBOutlet weak var pieChartView: PieChartView!
-
+    @IBOutlet weak var indicatorView: UIActivityIndicatorView!
+    @IBOutlet weak var labelStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.indicatorView.startAnimating()
         self.fetchCovidOverview(completionHandler: { [weak self] result in
             guard let self = self else {return}
+            self.indicatorView.stopAnimating()
+            self.indicatorView.isHidden = true
+            self.labelStackView.isHidden = false
+            self.pieChartView.isHidden = false
             switch result {
             case let .success(result):
+                self.configureStackView(koreaCovidOverview: result.korea)
+                let covidOverviewList = self.makeCovidOverviewList(cityCovidOverview: result)
+                self.configureChartView(covidOverviewList: covidOverviewList)
                 debugPrint("success \(result)")
             case let .failure(error):
                 debugPrint("success \(error)")
@@ -30,6 +40,56 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func makeCovidOverviewList(cityCovidOverview: CityCovidOverview) -> [CovidOverview]{
+        return [
+            cityCovidOverview.seoul,
+            cityCovidOverview.busan,
+            cityCovidOverview.daegu,
+            cityCovidOverview.incheon,
+            cityCovidOverview.gwangju,
+            cityCovidOverview.daejeon,
+            cityCovidOverview.ulsan,
+            cityCovidOverview.sejong,
+            cityCovidOverview.gyeonggi,
+            cityCovidOverview.chungbuk,
+            cityCovidOverview.chungnam,
+            cityCovidOverview.gyeongbuk,
+            cityCovidOverview.gyeongnam,
+            cityCovidOverview.jeju,
+        ]
+    }
+    
+    func configureChartView(covidOverviewList: [CovidOverview]){
+        self.pieChartView.delegate = self
+        let entries = covidOverviewList.compactMap({[weak self] overview -> PieChartDataEntry? in
+            guard let self = self else {return nil}
+            return PieChartDataEntry(value: self.removeFormatString(string: overview.newCase), label: overview.countryName, data: overview)
+        })
+        let dataSet = PieChartDataSet(entries: entries, label: "코로나 발생 현황")
+        dataSet.sliceSpace = 1
+        dataSet.entryLabelColor = .black
+        dataSet.xValuePosition = .outsideSlice
+        dataSet.valueTextColor = .black
+        dataSet.valueLinePart1OffsetPercentage = 0.8
+        dataSet.valueLinePart1Length = 0.2
+        dataSet.valueLinePart2Length = 0.3
+        
+        dataSet.colors = ChartColorTemplates.vordiplom() + ChartColorTemplates.joyful() + ChartColorTemplates.liberty() + ChartColorTemplates.pastel() + ChartColorTemplates.material()
+        
+        self.pieChartView.data = PieChartData(dataSet: dataSet)
+        self.pieChartView.spin(duration: 0.3, fromAngle: self.pieChartView.rotationAngle, toAngle: self.pieChartView.rotationAngle + 80)
+    }
+    
+    func removeFormatString(string: String) -> Double{
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        return formatter.number(from: string)?.doubleValue ?? 0
+    }
+    
+    func configureStackView(koreaCovidOverview: CovidOverview){
+        self.totalCaseLabel.text = "\(koreaCovidOverview.totalCase)명"
+        self.newCaseLabel.text = "\(koreaCovidOverview.newCase)명"
+    }
 
     func fetchCovidOverview(
         completionHandler: @escaping (Result<CityCovidOverview, Error>) -> Void
@@ -57,3 +117,11 @@ class ViewController: UIViewController {
     }
 }
 
+extension ViewController: ChartViewDelegate{
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        guard let covidDetailViewController = self.storyboard?.instantiateViewController(identifier: "CovidDetailViewController") as? CovidDetailViewController else {return}
+        guard let covidOverview = entry.data as? CovidOverview else {return}
+        covidDetailViewController.covidOverview = covidOverview
+        self.navigationController?.pushViewController(covidDetailViewController, animated: true)
+    }
+}
