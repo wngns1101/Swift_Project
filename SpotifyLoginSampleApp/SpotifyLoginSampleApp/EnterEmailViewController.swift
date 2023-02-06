cd975e4a53afa1acf9e4efeb520acd56a4818220
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 <wngns0812@gmail.com>
2023-02-06 15:52:10 +0900

    firebase login and logout function

--- SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmailViewController.swift
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmailViewController.swift
@@ -34,10 +34,20 @@ class EnterEmailViewController: UIViewController{
         let password = passwordTextField.text ?? ""
         
         //신규 사용자 생성
-        Auth.auth().createUser(withEmail: email, password: password){ [weak self]authResult, error in
+        Auth.auth().createUser(withEmail: email, password: password){ [weak self] authResult, error in
             guard let self = self else {return}
             
-            self.showMainViewController()
+            if let error = error{
+                let code = (error as NSError).code
+                switch code {
+                case 17007: //이미 가입한 계정일 때
+                    self.loginUser(withEmail: email, password: password)
+                default:
+                    self.errorMessageLabel.text = error.localizedDescription
+                }
+            }else{
+                self.showMainViewController()
+            }
             
         }
     }
@@ -47,6 +57,18 @@ class EnterEmailViewController: UIViewController{
         let mainViewController = storyboard.instantiateViewController(withIdentifier: "MainViewController")
         mainViewController.modalPresentationStyle = .fullScreen
         navigationController?.show(mainViewController, sender: nil)
+    }
+    
+    private func loginUser(withEmail email: String, password: String){
+        Auth.auth().signIn(withEmail: email, password: password) {[weak self] _, error in
+            guard let self = self else {return}
+            
+            if let error = error{
+                self.errorMessageLabel.text = error.localizedDescription
+            }else{
+                self.showMainViewController()
+            }
+        }
     }
 }
 
--- SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
@@ -26,6 +26,12 @@ class MainViewController: UIViewController {
         """
     }
     @IBAction func logoutButtonTapped(_ sender: UIButton) {
-        self.navigationController?.popToRootViewController(animated: true)
+        let firebaseAuth = Auth.auth()
+        do{
+            try firebaseAuth.signOut()
+            self.navigationController?.popToRootViewController(animated: true)
+        }catch let signOutError as NSError{
+            print("ERROR: signout \(signOutError.description)")
+        }
     }
 }
